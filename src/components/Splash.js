f81d13a64748107fb8a7defdbb05d29f41fe2273
TheQueepingHour <redex109@gmail.com>
2024-10-15 16:54:34 -0500

    Initialized components folder and added Splash.js for main landing page.

---
+++ src/components/Splash.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { Container } from "react-bootstrap";
+
+function Splash() {
+    return (
+        <Container>
+            <h1>
+                Welcome to SmashTingz
+            </h1>
+        </Container>
+    )
+}
+
+export default Splash
