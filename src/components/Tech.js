9ab29d9fa070b580b37e1a97d792aae7146a8d79
TheQueepingHour <redex109@gmail.com>
2024-10-22 18:00:05 -0500

    Edited basics for frames, added new class for distinguishing pink text and wiki text, added placeholder for Tech

--- src/components/Tech.js
+++ src/components/Tech.js
@@ -1,12 +1,27 @@
 import React from "react";
-import { Container } from "react-bootstrap";
+import { Container, Accordion } from "react-bootstrap";
 
 function Tech() {
     return (
         <Container fluid style={{paddingTop: '5em'}}>
-            <h1 className="blueText">
-                Welcome to the Tech page.
-            </h1>
+            <div className="mx-3 yellowText">
+                <h2>
+                    Welcome to the Tech page.
+                </h2>
+                <p>
+                    Here you will find all the Tech that I find to be useful/important.
+                </p>
+            </div>
+            <Container fluid>
+            <Accordion flush data-bs-theme="dark" className="techBGC">
+                <Accordion.Item eventKey="0">
+                    <Accordion.Header>PH</Accordion.Header>
+                    <Accordion.Body>
+                        more PH
+                    </Accordion.Body>
+                </Accordion.Item>
+            </Accordion>
+            </Container>
         </Container>
     )
 }
